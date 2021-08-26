d6e441518cfd1ad4e24d564fa7924f2ad9c2810e
SoniaSayago <75644975+SoniaSayago@users.noreply.github.com>
2021-08-26 08:48:31 -0500

    express router

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var http_1 = __importDefault(require("http"));
+// import http from "http";
 var dotenv_1 = __importDefault(require("dotenv"));
+var express_1 = __importDefault(require("express"));
 dotenv_1.default.config();
-http_1.default.createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Heroku world!");
-    response.end();
-}).listen(process.env.PORT);
+var expressApp = express_1.default();
+var port = process.env.PORT || 3000;
+expressApp.get("/", function (req, res) {
+    res.send("Hello World!");
+});
+expressApp.listen(port, function () {
+    console.log("Listening on port " + port);
+});
+// http.createServer(function(request: http.IncomingMessage, response: http.ServerResponse) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write("Heroku world!");
+//   response.end();
+// }).listen(process.env.PORT);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
-import http from "http";
+// import http from "http";
 import dotenv from "dotenv";
+import express from "express";
 dotenv.config()
 
-http.createServer(function(request: http.IncomingMessage, response: http.ServerResponse) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Heroku world!");
-  response.end();
-}).listen(process.env.PORT);
+const expressApp = express();
+const port = process.env.PORT || 3000;
+expressApp.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+expressApp.listen(port, () => {   
+    console.log(`Listening on port ${port}`);
+});
+
+
+// http.createServer(function(request: http.IncomingMessage, response: http.ServerResponse) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write("Heroku world!");
+//   response.end();
+// }).listen(process.env.PORT);
