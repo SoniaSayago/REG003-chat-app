6db55da96b2074317dfc5d4de7a978a2fa72a978
SoniaSayago <75644975+SoniaSayago@users.noreply.github.com>
2021-09-09 13:01:42 -0500

    1st users in postgrees & heroku

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,11 @@
-import{getUsers} from "../controller/user.controller";
+import{getUsers, newUser} from "../controller/user.controller";
 import { Application} from 'express';
 
 
 export default (app:Application, next:any) => {
   app.get('/users', getUsers);
   // // app.get('/users/:uid', requireAuth, getOneUser);
-  // app.post('/users', newUser);
+  app.post('/users', newUser);
 
   // app.put('/users/:uid', requireAuth, updateUser);
   // app.delete('/users/:uid', requireAuth, deleteOneUser);
